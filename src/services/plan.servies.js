072487e77eb144ff2c3f13675b3139fee4f418ba
noy2611 <noyr2611@gmail.com>
2024-02-29 11:54:31 +0200

    add getbyid

--- src/services/plan.servies.js
+++ src/services/plan.servies.js
@@ -5,7 +5,7 @@ const baseURL = "https://assignment2-server-cdgy.onrender.com";
 
 const instance = axios.create({
   baseURL,
-  timeout: 10000, // Adjust the timeout as needed
+  timeout: 10000, 
 });
 
 export const getAllPlans = async () => {
@@ -17,3 +17,14 @@ export const getAllPlans = async () => {
     throw error;
   }
 };
+
+
+export const getPlanById = async (id) => {
+    try {
+      const response = await instance.get(`/plan/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Error fetching plan with ID ${id}:`, error.message);
+      throw error;
+    }
+  };
